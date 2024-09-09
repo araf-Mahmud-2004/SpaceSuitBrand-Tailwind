21f45906fc5076e4c0eee9d45787a9a287dfb464
I-am-Araf-Mahmud <arafmahmud028@gmail.com>
2024-09-09 16:37:10 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mode: 'jit',  // Enable JIT mode
+    purge: ['./**/*.html'],  // Adjust the purge paths if necessary
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
